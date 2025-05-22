94f36d484797a39e8aca1f4a09d0177b0a59196f
Vishnu_Stony <vishnustony1615@gmail.com>
2025-05-22 15:54:04 +0530

    corrections

--- src/app/HomeComponent/Columns.tsx
+++ src/app/HomeComponent/Columns.tsx
@@ -15,14 +15,14 @@ export default function Columns() {
 
                 <div className="flex flex-col justify-center items-center pb-4 text-black text-center">
                     <div className="pb-3">
-                        <Image src="/home/high-quality-materials.png" alt="High Quality Material" width={70} height={40} className="pb-3" />
+                        <Image src="/home/high-quality-material1.png" alt="High Quality Material" width={70} height={40} className="pb-3" />
                     </div>
                     <h3 className="mt-2 font-black text-[16px] uppercase">High Quality Material</h3>
                 </div>
 
                 <div className="flex flex-col justify-center items-center pb-4 text-black text-center">
                     <div className="pb-3">
-                        <Image src="/home/dynotest2.png" alt="Dyno Tested" width={100} height={40} className="pb-3" />
+                        <Image src="/home/dynotest1.png" alt="Dyno Tested" width={100} height={40} className="pb-3" />
                     </div>
                     <h3 className="-top-3 relative font-black text-[16px] uppercase">Dyno Tested</h3>
                 </div>
