f0c530a399e9c5730ca53964c359641cd07c8889
Vishnu_Stony <vishnustony1615@gmail.com>
2025-05-26 12:31:13 +0530

    corrections

--- src/app/(pages)/shipping-policy/Components/Banner.tsx
+++ src/app/(pages)/shipping-policy/Components/Banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
     return (
         <>
-            <section className="pt-10 md:pt-40 shipping-bg">
+            <section className="shipping-bg">
 
                 <div className="bottom-[150px] left-[50px] z-2 absolute text-white">
                     <h3 className="pb-4 text-[40px]">Shipping Policy</h3>
